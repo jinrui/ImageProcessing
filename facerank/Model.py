acd6c6d6e5e6917c7de18d51f7a1717b4c0b4604
王曲颖 <wangquying@wangquyingdeMacBook-Pro.local>
2018-02-08 01:51:35 +0800

    complete the code ,then i will train the model

--- facerank/Model.py
+++ facerank/Model.py
@@ -20,29 +20,37 @@ files = os.listdir(filepath)
 filelist = [filepath+i for i in files]
 def handleImg():
     ylabel = pd.read_csv(ratepath)
+    #ylabel.dropna()
+    print(np.where(np.isnan(ylabel['#image'])))
+    ylabel['#image']=ylabel['#image'].apply(lambda x:int(x))
     ylabel1 = ylabel[['#image','Rating']].groupby(['#image']).median()#['Rating']
-    print(ylabel1.loc[1])
+    #print(ylabel1.loc[1])
+    print(ylabel['#image'].head())
     return ylabel1
 ylabel = handleImg()
-    
+print(ylabel.loc[46]['Rating'])    
 
-def getBatchImage(filelist,batch):
+def getBatchImage(batch):
     result = []
     yresult = []
     random.shuffle (filelist)
     for i in range(batch):
+        if not filelist[i].endwith('jpg'):
+            continue
         img = Image.open(filelist[i]).resize((128,128))
-        result.append(img)
-        num = filelist[i].split('-')[-1].split('.')[0]
-        y = ylabel.loc[num]
-        yla = np.zeros([1,5],0)
+        result.append(numpy.array(img)/255)
+        num = int(filelist[i].split('-')[-1].split('.')[0])
+        #print(num)
+        y = int(ylabel.loc[num]['Rating'])-1
+        yla = np.zeros(5)
         yla[y]=1
         yresult.append(yla)
-    return img,yresult
+        #result.append(img)
+    return result,yresult
 
 
 def weight_variable(shape):
-    return tf.Variable(tf.truncated_normal(shape),stddev=0.1)
+    return tf.Variable(tf.truncated_normal(shape,stddev=0.1))
 
 def bias_variable(shape):
     return tf.Variable(tf.constant(0.1,shape=shape))
@@ -72,22 +80,22 @@ def makesoftnet(x,w_shape,b_shape):
     return tf.nn.softmax(tf.matmul(x,w)+b)
 
 batch_size=16
-lr = 0.001
+lr = 0.0001
 epoch = 100
 
 x = tf.placeholder(tf.float32,[batch_size,128,128,3])
 y = tf.placeholder(tf.float32,[batch_size,5])
-keep_prob = tf.placeholder(tf.float16)
+keep_prob = tf.placeholder(tf.float32)
 
-h1 = makenet(x,[5,5,3,32],[32])    
-h2 = makenet(h1,[5,5,32,64],[64])
+h1 = makenet(x,[5,5,3,16],[16])    
+h2 = makenet(h1,[5,5,16,32],[32])
 
 #full connected
-c3 = makefullnet(tf.reshape(h2,[-1,32*32*64]),[32*32*64,1024],[1024],keep_prob)
+c3 = makefullnet(tf.reshape(h2,[-1,32*32*32]),[32*32*32,1024],[1024],keep_prob)
 
-c4 = makesoftnet(c3,[1024,5],[5])
+c4 = makesoftnet(c3,[1024,5],[5])+1e-10
 
-cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(c4)),reduction_indices=1)
+cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(c4),reduction_indices=[1]))
 train = tf.train.AdamOptimizer(lr).minimize(cost)
 
 correct_prediction = tf.equal(tf.argmax(c4,1), tf.argmax(y,1)) # 计算准确度
@@ -95,6 +103,11 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
 with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
     for i in range(epoch):
         x_train,y_train=getBatchImage(batch_size)
-        acc,cos,_=sess.run([accuracy,cost,train],feed_dicg={x:x_train,y:y_train})
+      
+        #print(x_train,y_train)
+      
+        acc,cos,_=sess.run([accuracy,cost,train],feed_dict={x:x_train,y:y_train,keep_prob: 0.8})
+        print('epoch=%d,acc=%f,cos=%f'%(i,acc,cos))
